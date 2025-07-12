3975ab0c80347ff5daa073e84bc68fa9de3bbb42
badstar6733 <woranee87190@gmail.com>
2025-07-12 22:42:41 +0700

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "▶️ เริ่มต้น build script"
+
+# (ไม่จำเป็นในกรณีใช้ puppeteer ตัวเต็ม แต่เผื่อในอนาคต)
+# npm exec puppeteer browsers install chrome
+
+echo "✅ Build script เสร็จสิ้น"
